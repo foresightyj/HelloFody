9713d83cddab3db1846c942a915fa10c97c74a30
foresightyj <imyuanjian@gmail.com>
2017-08-20 13:57:42 +0800

    initial

---
+++ HelloFody/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloFody
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var p1 = new Person { Age = 10, Name = "Xixi" };
+            var p2 = new Person { Age = 10, Name = "Xixi" };
+
+            Console.WriteLine(p1 == p2);
+            Console.ReadKey();
+        }
+    }
+
+    [Equals]
+    public class Person
+    {
+        public int Age { get; set; }
+        public String Name { get; set; }
+    }
+}
